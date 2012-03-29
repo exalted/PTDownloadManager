334219d250cb7d0334dac875c42c88557e1a0bf5
Ali Servet Donmez <asd@pittle.org>
2012-03-29 10:30:38 +0200

    Initial commit

---
+++ Examples/DownloadManagerDemo/DownloadManagerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DownloadManagerDemo
+//
+//  Created by Ali Servet Donmez on 29.3.12.
+//  Copyright (c) 2012 Apex-net srl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PTAppDelegate class]));
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Intro
+=====
+
+Not much to see here, don't hold your breath until a good ol' README comes up! ;-)
