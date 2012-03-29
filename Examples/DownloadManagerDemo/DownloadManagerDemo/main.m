8b86d973f3662cf735393918dad03fa9200aaaa3
Ali Servet Donmez <asd@pittle.org>
2012-03-29 10:57:19 +0200

    Add copyright notice and statement of copying permission to each source file

--- Examples/DownloadManagerDemo/DownloadManagerDemo/main.m
+++ Examples/DownloadManagerDemo/DownloadManagerDemo/main.m
@@ -1,9 +1,19 @@
 //
-//  main.m
-//  DownloadManagerDemo
+// Copyright (C) 2012 Ali Servet Donmez. All rights reserved.
 //
-//  Created by Ali Servet Donmez on 29.3.12.
-//  Copyright (c) 2012 Apex-net srl. All rights reserved.
+// This file is part of PTDownloadManager.
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PTDownloadManager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PTDownloadManager. If not, see <http://www.gnu.org/licenses/>.
+// PTDownloadManager is free software: you can redistribute it and/or
 //
 
 #import <UIKit/UIKit.h>
